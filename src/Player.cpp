a00764e2a2876ecf5387f2d9bf6f19ed712d552f
Samriddhi Sinha <samriddhisinha52@gmail.com>
2024-11-07 02:09:42 +0530

    Adding Player.cpp

---
+++ src/Player.cpp
@@ -0,0 +1,24 @@
+#include "Player.h"
+#include <iostream>
+
+Player::Player(std::string name) : playerName(name), attemptsLeft(7) {}
+
+std::string Player::getName() const {
+    return playerName;
+}
+
+void Player::decrementAttempts() {
+    attemptsLeft--;
+}
+
+bool Player::hasAttemptsLeft() const {
+    return attemptsLeft > 0;
+}
+
+std::string Player::makeGuess() {
+    std::string guess;
+    std::cout << "Enter your guess: ";
+    std::cin >> guess;
+    return guess;
+}
+
