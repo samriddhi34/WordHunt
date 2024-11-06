3bee723b0846ba88054208ec18412d27241839bb
Samriddhi Sinha <samriddhisinha52@gmail.com>
2024-11-07 02:23:38 +0530

    Adding Player.h

---
+++ include/Player.h
@@ -0,0 +1,29 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+
+class Player {
+private:
+    std::string playerName;    // Name of the player
+    int attemptsLeft;          // Number of attempts remaining
+
+public:
+    // Constructor to initialize player name and attempts
+    Player(std::string name);
+
+    // Function to get the player's name
+    std::string getName() const;
+
+    // Function to decrement the attempts
+    void decrementAttempts();
+
+    // Function to check if the player has any attempts left
+    bool hasAttemptsLeft() const;
+
+    // Function to make a guess
+    std::string makeGuess();
+};
+
+#endif
+
