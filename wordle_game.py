89fec8e2da0e6d4b76201e7a3bcfb94028e9f14a
Samriddhi Sinha <samriddhisinha52@gmail.com>
2024-12-13 17:01:08 +0530

    Adding the logic to enter guess word and check if it is valid or not

--- wordle_game.py
+++ wordle_game.py
@@ -8,8 +8,21 @@ def load_words(file_path):
 def choose_random_word(words):
     return random.choice(words)
 
+
+def get_player_guess():
+    guess = input("Enter a 5-letter word: ").strip().lower()
+    while len(guess) != 5 or not guess.isalpha():
+        print("Invalid input! Please enter a valid 5-letter word.")
+        guess = input("Enter a 5-letter word: ").strip().lower()
+    return guess
+
+
 words = load_words('words.txt')
 secret_word = choose_random_word(words)
 print(f"Secret word is: {secret_word}")  # For testing
+
+guess = get_player_guess()
+print(f"Your guess: {guess}")
+
 
 
